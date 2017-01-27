325b3fd54c47568decd3a6bb8f30963ee6f1ec58
Samy <zobiri_s@esme.fr>
2017-01-27 11:16:24 +0100

    airbnb maps installed

--- Application/Components/HomePage/HomePage.js
+++ Application/Components/HomePage/HomePage.js
@@ -4,15 +4,43 @@
 
 import React, {Component} from 'react';
 import {Text, Navigator, TouchableHighlight, StyleSheet, View} from 'react-native';
+import MapView from 'react-native-maps';
 
 export default class HomePage extends Component {
 
     render() {
         return (
-            <Text>Page carte</Text>
+            <View style={styles.container}>
+                <MapView
+                    style={styles.map}
+                    initialRegion={{
+                        latitude: 37.78825,
+                        longitude: -122.4324,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421,
+                    }}
+                />
+            </View>
         );
 
 
     }
 
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    map: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        height: 200,
+        width: window.width
+    },
+});
