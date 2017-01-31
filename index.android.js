953278da29937a8edbd48eadfd6f9dc4df638594
Samy <zobiri_s@esme.fr>
2017-01-31 21:12:26 +0100

    generate guid and store it

--- index.android.js
+++ index.android.js
@@ -4,10 +4,11 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
     AppRegistry,
     StyleSheet,
+    AsyncStorage,
     Text,
     View,
 } from 'react-native';
@@ -15,7 +16,34 @@ import {
 import Navigation from './Application/Router/Navigation'
 
 export default class app_babyfoot extends Component {
+
+    /*Function to generate guid*/
+    guid() {
+        function s4() {
+            return Math.floor((1 + Math.random()) * 0x10000)
+                .toString(16)
+                .substring(1);
+        }
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+            s4() + '-' + s4() + s4() + s4();
+    }
+
     render() {
+
+        try {
+            AsyncStorage.getItem('guid', (err, result) => {
+                 /*If no guid yet, then create it */
+                if (result==null) {
+                    var guid = this.guid();
+                    /*We stock the guid in the local storage*/
+                    AsyncStorage.setItem('guid', guid);
+                } else {
+                }
+            });
+        } catch (error) {
+            // Error retrieving data
+        }
+
         return (
             <Navigation />
         );
